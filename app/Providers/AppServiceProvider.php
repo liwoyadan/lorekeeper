15a72bb2c129c9eaeb643c294241df2da22b3127
min <liwoyadans@gmail.com>
2024-11-03 18:16:44 -0800

    refactor: last regex edit i swear.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,7 +42,7 @@ class AppServiceProvider extends ServiceProvider {
             // Currently this is just the layout blades, user list, pages blades, auth blades, and widgets.
             // Also excludes admin blades because those have an incredibly long sidebar.
             // This means if you have any sidebars on the front page i.e. featured character, it will display normally.
-            $noSidebar = '/layout\..*|browse.users|pages\..*|auth\..*|widgets\..*|admin\..*/i';
+            $noSidebar = '/layout\.|browse.users|pages\.|auth\.|widgets\.|admin\./i';
 
             if (preg_match_all($noSidebar, $fullName)) {
                 View::share('hasSidebar', false);
