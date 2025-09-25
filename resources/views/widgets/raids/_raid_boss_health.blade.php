6038491b8b717e1e586cf6d235bde63230a872b6
min <liwoyadans@gmail.com>
2025-09-25 02:21:40 -0700

    style: making raid boss info more legible

--- resources/views/widgets/raids/_raid_boss_health.blade.php
+++ resources/views/widgets/raids/_raid_boss_health.blade.php
@@ -1,14 +1,16 @@
-<div>
-    Has taken <b>{{ $raidBoss->damage }} points of damage</b> so far.
-</div>
-@if ($raidBoss->health)
+<div class="rounded p-2" style="background-color: rgba(255, 255, 255, 0.65);">
     <div>
-        <b>{{ $raidBoss->remainingHealth }} health</b> remains.
+        {!! $raidBoss->name !!} has taken <b>{{ $raidBoss->damage }} points of damage</b> so far.
     </div>
-    <div class="progress font-weight-bold" style="border: 2px solid var(--dark); height: 1.75rem; font-size: 1.15rem;">
-        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: {{ $raidBoss->remainingHealthPercentage }}%; {!! $raidBoss->barStyling ? $raidBoss->barStyling : '' !!}" aria-valuenow="{{ $raidBoss->remainingHealth }}" aria-valuemin="0"
-            aria-valuemax="{{ $raidBoss->health }}">
-            {{ $raidBoss->remainingHealth }} / {{ $raidBoss->health }}
+    @if ($raidBoss->health)
+        <div>
+            <b>{{ $raidBoss->remainingHealth }} health</b> remains.
         </div>
-    </div>
-@endif
+        <div class="progress font-weight-bold" style="border: 2px solid var(--dark); height: 1.75rem; font-size: 1.15rem;">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: {{ $raidBoss->remainingHealthPercentage }}%; {!! $raidBoss->barStyling ? $raidBoss->barStyling : '' !!}" aria-valuenow="{{ $raidBoss->remainingHealth }}" aria-valuemin="0"
+                aria-valuemax="{{ $raidBoss->health }}">
+                {{ $raidBoss->remainingHealth }} / {{ $raidBoss->health }}
+            </div>
+        </div>
+    @endif
+</div>
