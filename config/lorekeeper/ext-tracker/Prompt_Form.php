2e76f2db3bb86ec8d8769cd27dbb4554349971b7
liwoyadan <liwoyadan@users.noreply.github.com>
2025-10-17 06:56:57 +0000

    refactor: fix PHP styling

--- config/lorekeeper/ext-tracker/Prompt_Form.php
+++ config/lorekeeper/ext-tracker/Prompt_Form.php
@@ -1,10 +1,11 @@
 <?php
+
 return [
-    'key' => 'prompt_form',
+    'key'      => 'prompt_form',
     'wiki_key' => 'Prompt_Form',
-        'creators' => json_encode([
+    'creators' => json_encode([
         'MarskyMessier' => 'https://github.com/MarskyMessier',
-        'notecardPasta' => 'https://github.com/notecardPasta'
+        'notecardPasta' => 'https://github.com/notecardPasta',
     ]),
     'version' => '1.0.0',
 ];
--- database/migrations/2024_05_30_234338_add_prompt_form.php
+++ database/migrations/2024_05_30_234338_add_prompt_form.php
@@ -4,27 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('prompts', function(Blueprint $table) {
+    public function up(): void {
+        Schema::table('prompts', function (Blueprint $table) {
             $table->text('form')->nullable()->default(null);
-
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('prompts', function(Blueprint $table) {
+    public function down(): void {
+        Schema::table('prompts', function (Blueprint $table) {
             $table->dropColumn('form');
-
         });
     }
 };
