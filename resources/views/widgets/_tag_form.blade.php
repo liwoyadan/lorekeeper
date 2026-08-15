2fc3466c19a8758dddaa7feb41f0b7670ab59ddb
liwoyadan <liwoyadan@users.noreply.github.com>
2026-08-15 20:15:53 +0000

    refactor: fix blade formatting

--- resources/views/widgets/_tag_form.blade.php
+++ resources/views/widgets/_tag_form.blade.php
@@ -1,10 +1,10 @@
 @if ($model->exists)
     <h3>Tags</h3>
     <p>Add tags to help organize and filter this entry. Type to search existing tags or press Enter to create a new one.</p>
-    {!! Form::open(['url' => 'admin/tags/'.$model->tagSlug().'/'.$model->id]) !!}
-        @include('widgets._tag_input', ['model' => $model])
-        <div class="text-right">
-            {!! Form::submit('Save Tags', ['class' => 'btn btn-primary']) !!}
-        </div>
+    {!! Form::open(['url' => 'admin/tags/' . $model->tagSlug() . '/' . $model->id]) !!}
+    @include('widgets._tag_input', ['model' => $model])
+    <div class="text-right">
+        {!! Form::submit('Save Tags', ['class' => 'btn btn-primary']) !!}
+    </div>
     {!! Form::close() !!}
 @endif
